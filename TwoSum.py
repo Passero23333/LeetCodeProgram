2431ea2502f05f9e85b2f3970004af96ca0b4c36
Passero23333 <passero_23333@126.com>
2019-08-03 07:44:44 +0800

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,20 +1,7 @@
 class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        if len(nums) <= 1:
-            return False
-        for i in range(len(nums) - 1, 0, -1):
-            if (target - nums[i]) in nums[:i]:
-                print([nums.index(target - nums[i]), i])
-
-
-# test code
-nums = [2, 7, 11, 15]
-target = 9
-
-s = Solution()
-s.twoSum(nums, target)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(0, n):
+            difference = target - nums[i]
+            if difference in nums and nums.index(difference) != i:
+                return [i, nums.index(difference)]
